a6f45f5495d14ace8e7099201dca7acec59656e8
AcademeForge <academeforge@gmail.com>
2026-06-03 19:06:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,55 @@
-const CACHE_NAME = "academeforge-v1";
+const CACHE_NAME = "academeforge-v3";
 
-const STATIC_ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+const PRECACHE = [
+  "./",
+  "./index.html",
+  "./Y.html",
+  "./manifest.json",
+
+  "./account/index.html",
+  "./backup 1/index.html",
+  "./core/index.html",
+  "./flo/index.html",
+  "./sudoku-master/index.html",
+
+  "./AF LOGO 1.jpeg",
+  "./AF LOGO 2.jpeg",
+
+  "./banner1.png",
+  "./banner2.png",
+  "./Banner3.png",
+
+  "./Course 1.jpeg",
+  "./Course 2.jpeg",
+  "./Course 3.jpeg",
+  "./Course 4.jpeg",
+  "./Course 5.jpeg"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(STATIC_ASSETS);
-    })
-  );
-
   self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRECACHE))
+  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(
+          keys.map(key => {
+            if (key !== CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        )
+      ),
+      self.clients.claim()
+    ])
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -37,20 +57,26 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return (
-        cached ||
-        fetch(event.request)
-          .then(response => {
+
+      const networkFetch = fetch(event.request)
+        .then(response => {
+
+          if (
+            response &&
+            response.status === 200 &&
+            response.type === "basic"
+          ) {
             const clone = response.clone();
 
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, clone);
-            });
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, clone));
+          }
 
-            return response;
-          })
-          .catch(() => cached)
-      );
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || networkFetch;
     })
   );
 });
